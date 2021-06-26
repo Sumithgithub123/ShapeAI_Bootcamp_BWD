584e31c87e010b3ce5cf7f8dd2a438ccffca19ed
Sumithgithub123 <86525229+Sumithgithub123@users.noreply.github.com>
2021-06-26 20:19:22 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>About my Website</h1>
+      <p>This is my first website i made</p>
+    </div>
+  );
+}
+
+export default Info;
